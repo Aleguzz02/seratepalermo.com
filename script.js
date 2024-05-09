d75d9aa4cb6081b785c56f7fd26700245f5de65b
Aleguzz02 <alessandroguzzardi2002@gmail.com>
2024-05-09 18:06:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Events
 var eventsData = [
     { name: "AlbertiniBeachClub", lat: 38.15992, lng: 13.08529, date: "2024-05-11", org: "Fireworks" },
-    { name: "Vintage", lat: 38.1361984, lng: 13.3317784, date: "2024-05-11", org: "Habitat"},
-    { name: "Wave", lat: 38.1461984, lng: 13.3217784, date: "2024-05-12", org: "Testa" },
+    { name: "HypeClub", lat: 38.157069726710084, lng: 13.326524121981775, date: "2024-05-11", org: "Habitat"},
+    { name: "MOB", lat: 38.16178943269464, lng: 13.165000936024295, date: "2024-05-18", org: "Remember" },
     // ... more event data
     { name: "All day", lat: 38.090, lng: 13.340, date: "all" }
 ];
